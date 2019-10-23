bc304e37f5b7fc394341191575ad2275c946f486
周若晴 <cing@zhouruoqingde-MacBook-Pro.local>
2019-10-23 16:19:16 +0800

    10/23 1600

--- LeiShan_Practice1.js
+++ LeiShan_Practice1.js
@@ -5,12 +5,16 @@ function Add() {
     sessionStorage.setItem("phone", document.getElementById("phone").value);
 
     var table = document.getElementById("myTable");
-    var row = table.insertRow(2);
+    var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     cell1.innerHTML = sessionStorage.name;
     cell2.innerHTML = sessionStorage.phone;
+    cell3.innerHTML = "<td><a class='add' title='Add data-toggle='tooltip'><i class='material-icons'>&#xE03B;</i></a>"+
+        "<a class='edit' title='Edit' data-toggle='tooltip'><i class='material-icons'>&#xE254;</i></a>"+
+        "<a class='delete' title='Delete' data-toggle='tooltip'><i class='material-icons'>&#xE872;</i></a>"+
+        "</td>";
 
     document.getElementById("name").value="";
     document.getElementById("phone").value="";
@@ -28,40 +32,35 @@ $(document).ready(function(){
 	$('[data-toggle="tooltip"]').tooltip();
 	var actions = $("table td:last-child").html();
 
-
-	// Append table with add row form on add new button click
-    $("Add").click(function(){
-		$(this).attr("disabled", "disabled");
-		var index = $("table tbody tr:last-child").index();
-        var row = '<tr>' +
-            '<td><input type="text" class="form-control" name="name" id="name"></td>' +
-            '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
-			'<td>' + actions + '</td>' +
-        '</tr>';
-    	$("table").append(row);		
-		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-
 	// Add row on add button click
 	$(document).on("click", ".add", function(){
-        
-        sessionStorage.name = "111";
-        sessionStorage.phone = "222";
-
-      var table = document.getElementById("myTable");
-      var row = table.insertRow(1);
-      var cell1 = row.insertCell(0);
-      var cell2 = row.insertCell(1);
-      var cell3 = row.insertCell(2);
-      cell1.innerHTML = sessionStorage.name;
-      cell2.innerHTML = sessionStorage.phone;
-      var cell3 = '<td>' + actions + '</td>';
+		var empty = false;
+		var input = $(this).parents("tr").find('input[type="text"]');
+        input.each(function(){
+			if(!$(this).val()){
+				$(this).addClass("error");
+				empty = true;
+			} else{
+                $(this).removeClass("error");
+            }
+		});
+		$(this).parents("tr").find(".error").first().focus();
+		if(!empty){
+			input.each(function(){
+				$(this).parent("td").html($(this).val());
+			});			
+			$(this).parents("tr").find(".add, .edit").toggle();
+			$(".add-new").removeAttr("disabled");
+		}		
     });
 
 	// Edit row on edit button click
-	$(document).on("click", ".edit", function(){
-
+	$(document).on("click", ".edit", function(){		
+        $(this).parents("tr").find("td:not(:last-child)").each(function(){
+			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+		});		
+		$(this).parents("tr").find(".add, .edit").toggle();
+		$(".add-new").attr("disabled", "disabled");
     });
 
 	// Delete row on delete button click
